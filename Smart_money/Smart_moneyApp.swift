8abb336f2672af9f7354b40059acb8a4096aba9a
Andrey <fibiknew@gmail.com>
2026-02-14 23:38:45 +0200

    Initial Commit

---
+++ Smart_money/Smart_moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_moneyApp.swift
+//  Smart_money
+//
+//  Created by Filobokov Andrii on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Smart_moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
